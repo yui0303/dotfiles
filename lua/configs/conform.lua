4d457f13af38dd4b7b1abb0924908e4370d62db9
yui0303 <iampeter62@gmail.com>
2024-08-05 01:18:14 +0800

    chore: disable auto format on save

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -12,11 +12,11 @@ local options = {
       prepend_args = { "-style=google" },
     },
   },
-  format_on_save = {
-    -- These options will be passed to conform.format()
-    timeout_ms = 500,
-    lsp_fallback = true,
-  },
+  -- format_on_save = {
+  --   -- These options will be passed to conform.format()
+  --   timeout_ms = 500,
+  --   lsp_fallback = true,
+  -- },
 }
 
 require("conform").setup(options)
